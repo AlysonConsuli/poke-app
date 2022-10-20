aab33999dd8c1955e1c16e3d548ea0da05818eba
alyson <consulialyson@gmail.com>
2022-10-20 18:23:47 -0300

    feat: form to search pokemon

---
+++ src/components/Form.vue
@@ -0,0 +1,35 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <p>
+      <input
+        placeholder="nome do pokemon"
+        type="text"
+        name="pokemon"
+        v-model="pokemon"
+      />
+    </p>
+    <button v-on:click="searchPokemon" type="submit">Pesquisar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pokemon: "",
+    };
+  },
+  methods: {
+    searchPokemon() {
+      if (this.pokemon.trim() === "") {
+        return;
+      }
+      this.$emit("show-pokemon", {
+        pokemon: this.pokemon,
+      });
+      this.pokemon = "";
+    },
+  },
+};
+</script>
